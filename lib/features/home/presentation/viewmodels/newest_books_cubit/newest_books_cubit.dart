8b4d79fd078244c142e7e05b572d79cff6384838
HadiHeikal <hadiiheikal@gmail.com>
2026-05-18 15:31:40 +0300

    feat: initialize NewestBooks BLoC cubits and states

---
+++ lib/features/home/presentation/viewmodels/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:bloc/bloc.dart';
+import 'package:book_pulse_app/features/home/data/models/book_model.dart';
+import 'package:book_pulse_app/features/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+  // dependency injection using constructor injection
+  final HomeRepo homeRepo;
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold(
+      onSuccess: (booksList) {
+        emit(NewestBooksSuccess(books: booksList));
+      },
+      onFailure: (apiError) {
+        emit(NewestBooksFailure(errorMessage: apiError.message));
+      },
+    );
+  }
+}
