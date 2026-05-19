81cccdc26729cb5b86cc8b97213afb94c5ec2761
HadiHeikal <hadiiheikal@gmail.com>
2026-05-19 15:17:21 +0300

    feat: register home feature cubits in MultiBlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:book_pulse_app/core/constants/app_colors.dart';
+import 'package:book_pulse_app/core/services/api_service_locaor.dart';
+import 'package:book_pulse_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:book_pulse_app/features/home/presentation/viewmodels/best_seller_cubit/best_seller_cubit.dart';
+import 'package:book_pulse_app/features/home/presentation/viewmodels/featured_books_cubit/featured_book_cubit.dart';
+import 'package:book_pulse_app/features/home/presentation/viewmodels/newest_books_cubit/newest_books_cubit.dart';
+import 'package:book_pulse_app/features/home/presentation/viewmodels/top_rated_cubit/top_rated_cubit.dart';
 import 'package:book_pulse_app/routes/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const BookPulse());
@@ -11,13 +18,37 @@ class BookPulse extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: 'BookPulse',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: AppColors.mainColor,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              FeaturedBookCubit(homeRepo: getIt.get<HomeRepoImpl>())
+                ..fetchFeaturedBooks(),
+        ),
+        BlocProvider(
+          create: (context) =>
+              NewestBooksCubit(homeRepo: getIt.get<HomeRepoImpl>())
+                ..fetchNewestBooks(),
+        ),
+        BlocProvider(
+          create: (context) =>
+              TopRatedCubit(homeRepo: getIt.get<HomeRepoImpl>())
+                ..fetchTopRatedBooks(),
+        ),
+        BlocProvider(
+          create: (context) =>
+              BestSellerCubit(homeRepo: getIt.get<HomeRepoImpl>())
+                ..fetchBestSellerBooks(),
+        ),
+      ],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        title: 'BookPulse',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: AppColors.mainColor,
+        ),
+        routerConfig: AppRouter.router,
       ),
-      routerConfig: AppRouter.router,
     );
   }
 }
