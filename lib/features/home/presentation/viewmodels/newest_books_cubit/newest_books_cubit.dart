27ea5c5dccf5f6c75e0c1f587e53e64729b439e8
HadiHeikal <hadiiheikal@gmail.com>
2026-05-19 15:18:27 +0300

    feat: edit the imports of flutter_bloc package for best seller, featured, newest, and top rated book retrieval

--- lib/features/home/presentation/viewmodels/newest_books_cubit/newest_books_cubit.dart
+++ lib/features/home/presentation/viewmodels/newest_books_cubit/newest_books_cubit.dart
@@ -1,4 +1,4 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:book_pulse_app/features/home/data/models/book_model.dart';
 import 'package:book_pulse_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
