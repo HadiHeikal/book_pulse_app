74aff43c0b63af7321f7c758142e312687b715a2
HadiHeikal <hadiiheikal@gmail.com>
2026-05-20 05:51:51 +0300

    feat: integrate BlocBuilder for dynamic book loading in ContinueReadingSection

--- lib/features/home/presentation/views/widgets/continue_reading_section.dart
+++ lib/features/home/presentation/views/widgets/continue_reading_section.dart
@@ -1,7 +1,10 @@
+import 'package:book_pulse_app/core/constants/app_colors.dart';
 import 'package:book_pulse_app/features/home/data/models/book_model.dart';
+import 'package:book_pulse_app/features/home/presentation/viewmodels/newest_books_cubit/newest_books_cubit.dart';
 import 'package:book_pulse_app/features/home/presentation/views/widgets/continue_reading_card.dart';
 import 'package:book_pulse_app/features/home/presentation/views/widgets/section_header.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gap/flutter_gap.dart';
 
 class ContinueReadingSection extends StatelessWidget {
@@ -18,11 +21,29 @@ class ContinueReadingSection extends StatelessWidget {
         const Gap(16),
         SizedBox(
           height: 240,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: books.length,
-            itemBuilder: (context, index) =>
-                ContinueReadingCard(book: books[index]),
+          child: BlocBuilder<NewestBooksCubit, NewestBooksState>(
+            builder: (context, state) {
+              if (state is NewestBooksFailure) {
+                return Center(
+                  child: Text(
+                    state.errorMessage,
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                );
+              } else if (state is NewestBooksLoading) {
+                return const Center(
+                  child: CircularProgressIndicator(color: AppColors.gold),
+                );
+              } else if (state is NewestBooksSuccess) {
+                return ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: state.books.length,
+                  itemBuilder: (context, index) =>
+                      ContinueReadingCard(book: state.books[index]),
+                );
+              }
+              return const SizedBox();
+            },
           ),
         ),
       ],
